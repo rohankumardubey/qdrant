2d1401077cd5c7f541025f38bade5deb4f719369
Andrey Vasnetsov <andrey@vasnetsov.com>
2022-01-11 14:57:16 +0100

    * Implement benchmarks for HNSW index search with different number of   stored points * Fix minor issue in HNSW graph edge assignment * Update profiler with call-graph report generation * Add profiling guide * Add HNSW graph statistics test function (debug inly)

--- lib/segment/benches/hnsw_search_graph.rs
+++ lib/segment/benches/hnsw_search_graph.rs
@@ -29,11 +29,7 @@ fn hnsw_benchmark(c: &mut Criterion) {
     for idx in 0..(NUM_VECTORS as PointOffsetType) {
         let added_vector = vector_holder.vectors[idx as usize].to_vec();
         let raw_scorer = vector_holder.get_raw_scorer(added_vector);
-        let scorer = FilteredScorer::new(
-            &raw_scorer,
-            &fake_condition_checker,
-            None,
-        );
+        let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
         let level = graph_layers.get_random_layer(&mut rng);
         graph_layers.link_new_point(idx, level, &scorer);
     }
@@ -43,11 +39,7 @@ fn hnsw_benchmark(c: &mut Criterion) {
             let query = random_vector(&mut rng, DIM);
 
             let raw_scorer = vector_holder.get_raw_scorer(query);
-            let scorer = FilteredScorer::new(
-                &raw_scorer,
-                &fake_condition_checker,
-                None,
-            );
+            let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
 
             graph_layers.search(TOP, EF, &scorer);
         })
@@ -58,11 +50,7 @@ fn hnsw_benchmark(c: &mut Criterion) {
             let query = random_vector(&mut rng, DIM);
 
             let raw_scorer = vector_holder.get_raw_scorer(query);
-            let scorer = FilteredScorer::new(
-                &raw_scorer,
-                &fake_condition_checker,
-                None,
-            );
+            let scorer = FilteredScorer::new(&raw_scorer, &fake_condition_checker, None);
 
             let mut iter = (0..NUM_VECTORS as PointOffsetType).into_iter();
             let mut top_score = 0.;
